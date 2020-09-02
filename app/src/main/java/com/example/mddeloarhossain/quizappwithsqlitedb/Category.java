aad06c7df52a2bab5fac372df5e7b208bc5183b8
MD. DELOAR HOSSAIN <deloar.pust@gmail.com>
2020-09-02 13:55:58 +0600

    Simple Quiz App

---
+++ app/src/main/java/com/example/mddeloarhossain/quizappwithsqlitedb/Category.java
@@ -0,0 +1,42 @@
+package com.example.mddeloarhossain.quizappwithsqlitedb;
+
+/**
+ * Created by MD. DELOAR HOSSAIN on 02-Sep-20.
+ */
+
+public class Category {
+    public static final int PROGRAMMING = 1;
+    public static final int GEOGRAPHY = 2;
+    public static final int MATH = 3;
+
+    private int id;
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return getName();
+    }
+}
